72a6f1e7e398cf8cb33ce901cd6b1bf53a7833ce
Eirik S. Morland <eirik@morland.no>
2015-06-07 10:22:15 +0200

    Remove the test file, fix some lint errors.

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var increase = 0;
 var decrease = 1;
 
 module.exports = function(obj) {
-  if (obj.type == increase) {
-    obj.count += .1;
-    if (obj.count > .9) {
+  if (obj.type === increase) {
+    obj.count += 0.1;
+    if (obj.count > 0.9) {
       obj.type = decrease;
     }
   }
   else {
-    obj.count -= .1;
-    if (obj.count < .1) {
+    obj.count -= 0.1;
+    if (obj.count < 0.1) {
       obj.type = increase;
       obj.count = 0;
     }
